702e4d16e4c890f762c5cead364216b78ced5928
codingproyek2026-beep <codingproyek2026@gmail.com>
2026-03-16 08:04:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,159 @@
+var pil= document.getElementById("pilihan");
+var prof= document.getElementById("prof");
+var artikel= document.getElementById("artikel");
+var galery= document.getElementById("galery");
+var video= document.getElementById("video");
+var pil2= document.getElementById("pilihan2");
+var kaki= document.getElementById("kaki");
+var bdn= document.getElementById("badan");
+var pil3= document.getElementById("pilihan3");
+var pil4= document.getElementById("pilihan4");
+var pil5= document.getElementById("pilihan5");
+var isi= document.getElementById("isi-galery");
+var isi2= document.getElementById("isi-galery2");
+var isi3= document.getElementById("isi-galery3");
+var isi4= document.getElementById("isi-galery4");
+var ision= document.getElementById("isi-one");
+var isini= document.getElementById("isi-nih");
+var galhe= document.getElementById("galhe");
+var back= document.getElementById("back");
+var back2= document.getElementById("back2");
+var back3= document.getElementById("back3");
+var back4= document.getElementById("back4");
+var galhe2= document.getElementById("galhe2");
+var galhe3= document.getElementById("galhe3");
+var galhe4= document.getElementById("galhe4")
+pil.addEventListener("click",function(){
+    kaki.style.display="block";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+pil2.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="block";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+pil3.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="block";
+    galery.style.display="none";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+pil4.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="block";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+pil5.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+    video.style.display="block";
+})
+galhe.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi.style.display="block";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+galhe2.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi2.style.display="block";
+    isi.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
+galhe3.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="block";
+    isi4.style.display="none";
+})
+galhe4.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="none";
+    video.style.display="none";
+    isi4.style.display="block";
+})
+back.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="block";
+    video.style.display="none";
+    isi.style.display="none";
+})
+back2.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="block";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+})
+back3.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="block";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+})
+back4.addEventListener("click",function(){
+    kaki.style.display="none";
+    prof.style.display="none";
+    artikel.style.display="none";
+    galery.style.display="block";
+    video.style.display="none";
+    isi.style.display="none";
+    isi2.style.display="none";
+    isi3.style.display="none";
+    isi4.style.display="none";
+})
